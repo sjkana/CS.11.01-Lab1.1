f0cb4a75fffa85d52e573f64f69f7ce7a87dc94b
ExoHeart <69839114+ExoHeart@users.noreply.github.com>
2023-08-31 14:16:51 +0800

    completed pages and dateable questions

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age:");
+        String age = scanner.nextLine();
+
+        int age2;
+        int age3 = Integer.parseInt(age);
+        System.out.println(age + " year olds should date someone who is at least " +  (7+(age3/2)) +  " years old.");
     }
+
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age:");
+        String age = scanner.nextLine();
 
+        int age2 = Integer.parseInt(age);
+
+        System.out.println(age2 + " year olds should read at least " + (100-age2) + " pages in a book before giving up.");
     }
 }
